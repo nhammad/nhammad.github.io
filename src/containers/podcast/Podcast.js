3c43f4fa555d321bf7cefbc7573908bf2d963f76
Neeha <neeha.hammad@gmail.com>
2020-03-16 21:49:23 +0000

    first commit

---
+++ src/containers/podcast/Podcast.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Podcast.css";
+import { podcastSection } from "../../portfolio";
+
+export default function Podcast() {
+  return (null
+    // <div className="main">
+    //   <div className="podcast-header">
+    //     <h1 className="podcast-header-title">{podcastSection.title}</h1>
+    //     <p className="subTitle podcast-header-subtitle">{podcastSection.subtitle}</p>
+    //   </div>
+    //   <div className="podcast-main-div">
+    //     {podcastSection.podcast.map(podcastLink => {
+    //       return (null
+    //         /*<div>
+    //           <iframe className= "podcast" src={podcastLink} frameborder="0" scrolling="no"></iframe>
+    //         </div>*/
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,208 @@
+/* Change this file to get your personal Porfolio */
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  title: "Hello, I'm Neeha Hammad 👋",
+  subTitle:
+    "An eager to learn student with a keen interest in the fields of Data Science and Software Development ✨🌟 Always willing to learn new tech-stack and work with a goal-oriented team!",
+  resumeLink: "https://drive.google.com/file/d/1uv28SQzr_i03yG2pNPLIxsZIUs_-sqSO/view?usp=sharing"
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/nhammad",
+  linkedin: "https://www.linkedin.com/in/neehahammad/",
+  email: "neeha.hammad@hotmail.com",
+  facebook: "https://www.facebook.com/neeha.hammad1"
+};
+
+const skillsSection = {
+  /* Your Skills Section  */
+  title: "What I Enjoy Doing",
+  subTitle: "While Python is my favorite, it's so hard to stick to one thing when there's so much to learn!",
+  skills: [
+    "⚡ Exploratory Data Analysis & Applying Visualization Techniques on Data",
+    "⚡ Building Machine Learning Models to Predict Behaviors",
+    "⚡ Designing Interactive Front-End Interfaces",
+    "⚡ Using C++ STL to Create Algorithms & Find Solutions to Problems",
+
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon */
+  /* https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "c",
+      fontAwesomeClassname: "fab fa-cuttlefish"
+    },
+    {
+      skillName: "c",
+      fontAwesomeClassname: "fab fa-cuttlefish"
+    }, 
+    {
+      skillName: "+",
+      fontAwesomeClassname: "fas fa-plus"
+    },
+    {
+      skillName: "+",
+      fontAwesomeClassname: "fas fa-plus"
+    },    
+    {
+      skillName: "linux",
+      fontAwesomeClassname: "fab fa-linux"
+    },
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "React",
+      fontAwesomeClassname: "fab fa-react"
+    },
+  ]
+};
+
+const openSource = {
+  /* Your Open Source Section to View Your Github Pinned Projects */
+  /* To know how to get github key look at readme.md */
+
+  githubConvertedToken: "Your Github Converted Token",
+  githubUserName: "nhammad"
+};
+
+const bigProjects = {
+  /* Some Big Projects You have worked with your company */
+  title: "What Else I've Been Upto",
+  subtitle: "Education: Jacobs University Bremen - BSc. Computer Science",
+  projects: [
+    {
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
+      link: "https://assistant.google.com/services/a/uid/000000617e965fb2",
+      caption: "Google Assistant Action: Python Maestro"
+    },
+    {
+      image: "https://drive.google.com/uc?export=download&id=1FEbgRgWqZZXlHaqBMP3JXTK4AbnqN8ep",
+      link: "https://devpost.com/software/pengwin-7xw5nd",
+      caption: "HackZurich'19: PengWin"
+    }
+    ,
+    {
+      image: "https://drive.google.com/uc?export=download&id=13J6c_KXP0PqTMcotRgmuEBPwa5xC6hCF",
+      link: "https://www.facebook.com/projectagaahi/",
+      caption: "Agaahi: Creating Awareness"
+    },
+    
+  ]
+};
+/*https://drive.google.com/uc?export=download&id=128uZXpocpZf9g8armFsDrx3rVJl8EXlC*/
+const achievementSection = {
+  /* Your Achievement Section Include Your Certification Talks and More */
+
+  title: "Additional Certifications 🏆 ",
+  subtitle: "Including Interesting Courses That I took Up Online",
+
+  achivementsCards: [
+    {
+      title: "Data Scientist With Python",
+      description: "Including Courses like Cleaning Data, pandas Foundation, Supervised Learning with scikit-learn etc",
+      image:
+        "https://drive.google.com/uc?export=download&id=1E8Sum7oUYiqiMZHmd4qTW3SHjGK5WxM2",
+      footerLink: [
+        { name: "Certification", url: "https://www.datacamp.com/statement-of-accomplishment/track/8b20ebca9f5fbf09aa71b07a9fac5a6cf4d44fff" },
+       
+      ]
+    },
+    {
+      title: "SQL Fundamentals Track",
+      description: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: "https://drive.google.com/uc?export=download&id=1E8Sum7oUYiqiMZHmd4qTW3SHjGK5WxM2",
+      footerLink: [{ name: "Certification", url: "https://www.datacamp.com/statement-of-accomplishment/track/f73f34b01cd6eb6f93ae3890ffb85399656d2279" }]
+    },
+
+    {
+      title: "Gold Medal - International Essay Writing Competition ",
+      description: "Environment topc",
+      image: "https://drive.google.com/uc?export=download&id=1AHyo3Deol-2jtTCd6ijK0I4ILbR89ikQ",
+      footerLink: [
+        { name: "Certification", url: "" },
+        /*{name: "excercises", url: ""},*/
+      ]
+    }
+  ]
+};
+
+const blogSection = {
+  /* Blogs Section */
+
+  title: "Blogs",
+  subtitle: "Apart from playing around with different tech-stack, I also like to write and express my ideas. Check out some of my work here:",
+
+  blogs: [
+    {
+      url: "http://blogs.dunyanews.tv/20144/", 
+      image: "https://drive.google.com/uc?export=download&id=14PyDYRRWR--QMTa0pcfTDDidlp0vvY33",
+      title: "Take a peek inside the Arfa Kareem Technology Park ",
+      description: "Plan 9 is a technology incubator, which selects about ten technology start-ups twice a year, after a rigorous competition."
+    },
+    {
+      url: "https://nation.com.pk/02-Apr-2017/volunteering-at-a-low-cost-private-school-made-me-feel-blessed-here-s-why-you-should-do-it-too",
+      image: "https://drive.google.com/uc?export=download&id=1O61vmofPezJCOWSkTZnevblM13Gkq2f-",
+      title: "Volunteering At a Low Cost Private School",
+      description: "I was enraged when a teacher pulled one of my female students out of the class because she wanted someone to take care of her child."
+    },
+    {
+      url: "https://nation.com.pk/15-Aug-2019/670638", 
+      image: "https://drive.google.com/uc?export=download&id=1uTs-aBcuUN_QTOq9csCyGfjy96PgvMkc",
+      title: "Book Review: Three Daughters of Eve",
+      description: "The book is a mesmerising journey into romance, and religion"
+    },
+  ]
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "",
+  subtitle: "",
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: "",
+      image:
+        ""
+    }
+  ]
+};
+
+
+
+const contactInfo = {
+  title: "Contact Me ☎️",
+  subtitle: "Believe I have more potential than what my resume shows? Or simply want to say a hello? Get in touch!",
+  number: "+4915155705438",
+  email_address: "neeha.hammad@hotmail.com"
+};
+
+export { greeting, socialMediaLinks, skillsSection, openSource, bigProjects, achievementSection, blogSection, talkSection, contactInfo };
